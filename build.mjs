86958a8c19babe96944d27da427f631ef072cb0f
xxvabbasxxv-sys <xxvabbasxxv@gmail.com>
2026-09-19 22:28:49 +0300

    Delete build.mjs

--- build.mjs
+++
@@ -1,4 +0,0 @@
-import { mkdir, copyFile } from 'node:fs/promises';
-await mkdir('dist', { recursive: true });
-await copyFile('index.html', 'dist/index.html');
-console.log('Built dist/index.html');
